44b49096b981a688225275e4ecf43facb2414bd3
Sopan Ghorpade <sopan1916000070@gmail.com>
2024-10-19 13:32:38 +0530

    Make Some Changes

--- src/componant/Form.jsx
+++ src/componant/Form.jsx
@@ -1,36 +1,55 @@
 import React, { useState } from "react";
 
 export default function Form() {
-  // let [fullName, SetFullname] = useState("");
-  // let [userName, setUserName] = useState("");
+  let [formData, setFormData] = useState({ fullName: "", userName: "",password:"",});
+  let handleInputChange = (event) => {
+    let fieldName = event.target.name;
+    let newVal = event.target.value;
 
-  // const handleNameChange = (e) => {
-  //   SetFullname(e.target.value);
-  // };
-  // const handleUserChange = (e) => {
-  //   setUserName(e.target.value);
-  // };
-
-  let [formData, setFormData] = useState({ fullName: "", userName: "" });
+    setFormData((currData) => {
+      currData[fieldName] = newVal;
+      return { ...currData, [fieldName]: newVal };
+    });
+  };
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData );
+    
+    setFormData({ fullName: "", userName: "", password:"" });
+  };
   return (
     <div className="center form-container">
-      <label htmlFor="fullname">Name</label>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={fullName}
-        onChange={handleNameChange}
-        id="fullname"
-      />
-      <label htmlFor="username">username</label>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={userName}
-        onChange={handleUserChange}
-        id="username"
-      />
-      <button>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="fullname">Name</label>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={formData.fullName}
+          id="fullname"
+          name="fullName"
+          onChange={handleInputChange}
+          required
+        />
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={formData.userName}
+          id="username"
+          name="userName"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="password"
+          value={formData.password}
+          id="password"
+          name="password"
+          onChange={handleInputChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
